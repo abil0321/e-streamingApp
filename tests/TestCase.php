3dd835a8f34b08167ddb114bf6b636e92dea3556
Abil <salsabilmuhammad45@gmail.com>
2026-01-05 14:40:50 +0800

    fix: disable vite in tests to fix ci/cd

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    // Tambahkan method ini:
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // 🔥 INI KUNCINYA: Mematikan Vite saat testing
+        $this->withoutVite();
+    }
 }
